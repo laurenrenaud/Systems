0b1f07e8896868e669816fc0cd36c586b7ace43e
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-02-27 01:24:26 -0500

    added histo of percent removed

--- removed_products.R
+++ removed_products.R
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(ggplot2)
+library(RColorBrewer)
 options(scipen=4)
 
 inventory <- readr::read_csv("../data/Dec2016/biotrackthc_inventory.csv")
@@ -27,6 +29,23 @@ removedproduct <- inventory %>%
   # the month of the overall max date (aka report month) then assume closed
   mutate(status = ifelse(floor_date(maxDate, "month") < max(floor_date(locations$maxDate, "month"), na.rm=T),
                          "Closed", "Open"))
+#write.table(removedproduct, file="../data/Dec2016/cleaned/samples/removed_product.csc", row.names=F, sep=",")
+
+removedproduct$status <- factor(removedproduct$status, levels = c("Open", "Closed"))
+
+table(removedproduct$status)
+
+removedproduct %>%
+  filter(!is.na(status)) %>%
+  ggplot(aes(x=percremoved, color=status, fill=status)) +
+  geom_density(alpha=0.45) +
+  scale_fill_manual(values=brewer.pal(3,"Dark2")) +
+  #scale_color_manual(values=brewer.pal(3,"Dark2")) +
+  scale_color_manual(values=c("gray47", "gray47")) +
+  ggtitle("Percent of Product Removed \nby Store Status") +
+  ylab("Density") + xlab("Percent Removed") +
+  theme(legend.justification=c(1,1), legend.position=c(1,1))
+
 
 # entities with no removed product
 noremovals <- inventory %>%
