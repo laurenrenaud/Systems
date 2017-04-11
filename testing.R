e46594176a5b6b0b44ee4a1f369b88bc6d5d3b86
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-11 12:15:38 -0400

    updated classification to include juju joints, RSO, ESO

--- testing.R
+++ testing.R
@@ -18,15 +18,15 @@ colnames(inhalantnames) <- "retail_prodname"
 inhalantnames <- inhalantnames %>%
   dplyr::rowwise() %>%
   # boolean for cartridge
-  dplyr::mutate(cartridge = grepl("cart|vap|vc|pen|refill", retail_prodname, ignore.case = T),
+  dplyr::mutate(cartridge = grepl("cart|vap|vc|pen|refill|juju|joint", retail_prodname, ignore.case = T),
                 # extraction method
                 extraction = ifelse(grepl("BHO|butane", retail_prodname, ignore.case = T), "BHO",
                                     ifelse(grepl("CO2", retail_prodname, ignore.case = T), "CO2",
                                            "unknown"
                                     )),
                 # type
-                type = ifelse(grepl("cart|vap|vc|pen|refill", retail_prodname, ignore.case=T), "cartridge",
-                              ifelse(grepl("oil", retail_prodname, ignore.case=T), "oil",
+                type = ifelse(grepl("cart|vap|vc|pen|refill|juju|joint", retail_prodname, ignore.case=T), "cartridge",
+                              ifelse(grepl("oil|RSO|ESO", retail_prodname, ignore.case=T), "oil",
                                      ifelse(grepl("wax", retail_prodname, ignore.case=T), "wax",
                                             ifelse(grepl("hash", retail_prodname, ignore.case = T), "hash",
                                                    ifelse(grepl("kief|keif", retail_prodname, ignore.case = T), "kief",
@@ -37,14 +37,14 @@ inhalantnames <- inhalantnames %>%
                                                                                       "unknown")))))))
                               )),
                 # consumption method
-                consumption = ifelse(grepl("cart|vap|vc|pen|refill", retail_prodname, ignore.case = T), "vaping",
+                consumption = ifelse(grepl("cart|vap|vc|pen|refill|juju|joint", retail_prodname, ignore.case = T), "vaping",
                                      ifelse(grepl("wax|shatter|dab|resin", retail_prodname, ignore.case = T), "dabbing",
                                             ifelse(grepl("kief|keif|hash", retail_prodname, ignore.case = T), "smoking",
-                                                   ifelse(grepl("oil", retail_prodname, ignore.case = T), "oil",
+                                                   ifelse(grepl("oil|ESO|RSO", retail_prodname, ignore.case = T), "oil",
                                                           "unknown"))))
   )
 
-#write.table(inhalantnames, file="../data/Dec2016/cleaned/testing/inhalant_prodnames_dec2016.csv", row.names=F, sep=",")
+write.table(inhalantnames, file="../data/Dec2016/cleaned/testing/inhalant_prodnames_dec2016.csv", row.names=F, sep=",")
 
 # join classified inhalantnames back to dispening df
 solvents$productname <- as.factor(solvents$productname)
@@ -63,34 +63,39 @@ table(concentrates$solvent_tested, concentrates$extraction)
 prod_names <- solvents %>%
   dplyr::group_by(productname) %>%
   dplyr::summarise(ever_solvent_test = sum(solvent_tested) > 0,
-                   prods_sold = n()) %>%
+                   prods_sold = n(),
+                   cartridge = cartridge[1],
+                   extraction = extraction[1],
+                   type = type[1],
+                   consumption = consumption[1]
+                   ) %>%
                    #perc_inhalants = prods_sold / nrow(solvents)) %>%
   dplyr::mutate(inhalant_marketrank = rank(prods_sold)/ length(prods_sold)) %>%
-  dplyr::mutate(cartridge = grepl("cart|vap|vc|pen|refill", productname, ignore.case = T),
-                # extraction method
-                extraction = ifelse(grepl("BHO|butane", productname, ignore.case = T), "BHO",
-                                    ifelse(grepl("CO2", productname, ignore.case = T), "CO2",
-                                           "unknown"
-                                    )),
-                # type
-                type = ifelse(grepl("cart|vap|vc|pen|refill", productname, ignore.case=T), "cartridge",
-                              ifelse(grepl("oil", productname, ignore.case=T), "oil",
-                                     ifelse(grepl("wax", productname, ignore.case=T), "wax",
-                                            ifelse(grepl("hash", productname, ignore.case = T), "hash",
-                                                   ifelse(grepl("kief|keif", productname, ignore.case = T), "kief",
-                                                          ifelse(grepl("shatter", productname, ignore.case = T), "shatter",
-                                                                 ifelse(grepl("dab", productname, ignore.case = T), "dab",
-                                                                        ifelse(grepl("resin", productname, ignore.case = T), "resin",
-                                                                               ifelse(grepl("bubble", productname, ignore.case = T), "bubble",
-                                                                                      "unknown")))))))
-                              )),
-                # consumption method
-                consumption = ifelse(grepl("cart|vap|vc|pen|refill", productname, ignore.case = T), "vaping",
-                                     ifelse(grepl("wax|shatter|dab|resin", productname, ignore.case = T), "dabbing",
-                                            ifelse(grepl("kief|keif|hash", productname, ignore.case = T), "smoking",
-                                                   ifelse(grepl("oil", productname, ignore.case = T), "oil",
-                                                          "unknown"))))
-  ) %>%
+  # dplyr::mutate(cartridge = grepl("cart|vap|vc|pen|refill", productname, ignore.case = T),
+  #               # extraction method
+  #               extraction = ifelse(grepl("BHO|butane", productname, ignore.case = T), "BHO",
+  #                                   ifelse(grepl("CO2", productname, ignore.case = T), "CO2",
+  #                                          "unknown"
+  #                                   )),
+  #               # type
+  #               type = ifelse(grepl("cart|vap|vc|pen|refill", productname, ignore.case=T), "cartridge",
+  #                             ifelse(grepl("oil", productname, ignore.case=T), "oil",
+  #                                    ifelse(grepl("wax", productname, ignore.case=T), "wax",
+  #                                           ifelse(grepl("hash", productname, ignore.case = T), "hash",
+  #                                                  ifelse(grepl("kief|keif", productname, ignore.case = T), "kief",
+  #                                                         ifelse(grepl("shatter", productname, ignore.case = T), "shatter",
+  #                                                                ifelse(grepl("dab", productname, ignore.case = T), "dab",
+  #                                                                       ifelse(grepl("resin", productname, ignore.case = T), "resin",
+  #                                                                              ifelse(grepl("bubble", productname, ignore.case = T), "bubble",
+  #                                                                                     "unknown")))))))
+  #                             )),
+  #               # consumption method
+  #               consumption = ifelse(grepl("cart|vap|vc|pen|refill", productname, ignore.case = T), "vaping",
+  #                                    ifelse(grepl("wax|shatter|dab|resin", productname, ignore.case = T), "dabbing",
+  #                                           ifelse(grepl("kief|keif|hash", productname, ignore.case = T), "smoking",
+  #                                                  ifelse(grepl("oil", productname, ignore.case = T), "oil",
+  #                                                         "unknown"))))
+  # ) %>%
   arrange(extraction, ever_solvent_test, desc(inhalant_marketrank))
   
 table(prod_names$ever_solvent_test, prod_names$consumption)
