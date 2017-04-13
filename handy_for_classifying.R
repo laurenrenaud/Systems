e24e63cfec060c1b5ab9a4b252b78442b0d91a12
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-12 20:26:31 -0400

    some version of sample code for classifying

---
+++ handy_for_classifying.R
@@ -0,0 +1,40 @@
+# key words to regex function ----
+keyWordsToRegex <- function(key.words){
+  #' Takes a list of keywords and returns a regex 
+  #' string with each keyword seperated by a pipe 
+  #' indicating the terms are or'd together.
+  #' @param key.words   List of keywords
+  paste(key.words, collapse = "|")
+}
+# housing ------
+cartridge_string <- keyWordsToRegex(
+  c("cart", "refill", "vc", "vap", "pen", "juju", "joint")
+)
+
+
+## need to think about formatting grepl with | 
+## or
+## do we turn everything to lower
+## and then use %in% with " "
+ 
+
+categorizeSubtype <- function(subtype){
+  #' Takes product name and categorizes it into a 
+  #' product category type
+  #' @param subtype  A string of inhalant product names
+  #' @return A categorized usage of the productname as a string.
+  
+  # cartidge
+  if(grepl("cartridge_string", subtype, ignore.case=TRUE)){
+    return("Cartridge")
+  }
+  # Health
+  else (grepl("oil_string", subtype, ignore.case=TRUE)){
+    return("Oil")
+  }
+  else return("Unclassified")
+}
+
+# use categeories
+inhalantnames$Type <- as.character(sapply(as.character(inhalantnames$productname), categorizeSubtype))
+
