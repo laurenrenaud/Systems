2f4931ddac59e082f6543114bf7288f6976ea673
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-14 05:09:44 -0400

    converting for loop to function, modular

--- testing_partitionFunction.R
+++ testing_partitionFunction.R
@@ -4,12 +4,8 @@ oil_string <- "oil|RSO|ESO"
 categorizeProductName <- function(productName){
   #' Takes product name and categorizes it into a 
   #' product category type
-  #' @param subtype  A string of inhalant product names
+  #' @param productName  A string of inhalant product names
   #' @return A categorized usage of the productname as a string.
-  #' Takes a resource subtype and categorizes it into a 
-  #' Particular type of use of the resource
-  #' @param subtype  A string resource.hub subtype
-  #' @return A categorized usage of the resource as a string.
   
   # cartridge
   if(grepl(cartridge_string, productName, ignore.case=TRUE)){
