88dfabdf7e73d6ab3c11f59b86cb463edc1e555d
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-27 16:38:54 -0400

    re-clean extracts, buidling model

--- categorization_ML.R
+++ categorization_ML.R
@@ -2,6 +2,26 @@ library(dplyr)
 library(ggplot2)
 library(readr)
 
+# dispensing <- readr::read_csv("../data/Dec2016/biotrackthc_dispensing.csv")
+# dispensing$saledate <- as.Date(as.POSIXct(dispensing$sessiontime,
+#                                          origin = "1970-01-01", tz="America/Los_Angeles"))
+# inventory <- readr::read_csv("../data/Dec2016/biotrackthc_inventory.csv")
+
+extracts <- dispensing %>%
+  # 24 is inventory type for extracts
+  dplyr::filter(inventorytype==24) %>%
+  dplyr::select(dispensingid = id, weight, inventoryid, price, usableweight, saledate) %>%
+  dplyr::left_join(select(inventory, inventoryid = id, strain, productname, 
+                          inventoryparentid, sample_id),
+                   by="inventoryid") %>%
+  dplyr::mutate(price_x = ifelse(saledate >= "2015-07-01", 
+                                 price*1.37,
+                                 price),
+                price_per_gram = price_x/usableweight) %>%
+  dplyr::select(-(saledate), -(price))
+
+# write.table(extracts, file="../data/Dec2016/cleaned/samples/extracts.csv", sep=",", row.names=T)
+
 # pull in data -------
 extracts <- readr::read_csv("../data/Dec2016/cleaned/samples/inahlants.csv")
 
@@ -14,33 +34,6 @@ inv_types <- readr::read_csv("../data/Dec2016/cleaned/inventory_type.csv")
 tests <- rbind(micro, moisture, solvent)
 labkey <- readr::read_csv("../data/Dec2016/biotrackthc_labresults_samples.csv")
 labkey$inventoryid <- as.numeric(labkey$inventoryid)
-
-# drop duplicate inventoryparentid's
-labkey_parentids <- labkey %>%
-  dplyr::group_by(inventoryparentid) %>%
-  dplyr::summarise(count = n()) %>%
-  dplyr::filter(count == 1)
-labkey <- filter(labkey, inventoryparentid %in% labkey_parentids$inventoryparentid)
-
-# spread `tests`
-tests2 <- tests %>%
-  dplyr::left_join(select(labkey, id, inventoryparentid), by=c("sample_id" = "id")) %>%
-  dplyr::select(name, value, inventoryparentid) %>%
-  tidyr::spread(name, value)
-
-# spread tests manually
-tests_numeric <- tests %>%
-  dplyr::left_join(select(labkey, id, inventoryparentid), by=c("sample_id" = "id")) %>%
-  dplyr::group_by(inventoryparentid) %>%
-  dplyr::summarise(
-    micro = any(!is.na(value[name=="aerobic_bacteria"]))
-  )
-    
-    micro = (!is.na(aerobic_bacteria) | !is.na(yeast_and_mold) | !is.na(coliforms) |
-               !is.na(bile_tolerant) | !is.na(e_coli_and_salmonella)),
-    moisture = !is.na(moisture),
-    solvent = !is.na(residual_solvent)
-  )
 
 # get names from categorization function -----
 # first get list/df of inhalantnames
@@ -57,6 +50,122 @@ inhalantnames <- inhalantnames %>%
 # join classified inhalantnames back to dispening df
 extracts$productname <- as.factor(extracts$productname)
 extracts <- left_join(extracts, inhalantnames, by="productname")
+
+tests <- tests %>%
+  dplyr::left_join(select(labkey, id, inventoryparentid), by=c("sample_id" = "id")) %>%
+  dplyr::group_by(inventoryparentid, name) %>%
+  # getting max value to handle that some have multiple tests run,
+  # which may be error in the data
+  dplyr::summarise(
+    value = max(value)
+  ) %>%
+  tidyr::spread(name, value) %>%
+  dplyr::group_by(inventoryparentid) %>%
+  dplyr::summarise(
+    bacteria = !is.na(aerobic_bacteria),
+    bile = !is.na(bile_tolerant),
+    coliforms = !is.na(coliforms),
+    ecoli = !is.na(e_coli_and_salmonella),
+    yeast = !is.na(yeast_and_mold),
+    moisture = !is.na(moisture),
+    solvent = !is.na(residual_solvent)
+  ) 
+
+
+# convert to numeric from TRUE / FALSE
+tests_numeric <- tests * 1
+
+# get df of lab data types to add to test df
+lab_invtypes <- labkey %>%
+  group_by(inventoryparentid) %>%
+  # handles the 8 inventory types that have more than one type listed
+  summarise(inventorytype = inventorytype[1]) %>%
+  left_join(inv_types, by="inventorytype") %>%
+  select(-(inventorytype), lab_invtype = inv_type_name)
+
+# joining to the extracts df results in an increase in tests_numeric from
+# ~150k entries to ~2.5 million, which makes sense because a particular 
+# inventoryid may be linked to multiple retail products
+tests_numeric <- tests_numeric %>%
+  dplyr::left_join(select(extracts, inventoryparentid, price, CBD, Total,
+                          inhalant_type, inhalant_gen, productname),
+                   by="inventoryparentid") 
+# join the lab's inventory types back to the tests df
+tests_numeric <- tests_numeric %>%
+  dplyr::left_join(lab_invtypes, by="inventoryparentid") 
+
+
+# productname and therefore classification type are missing from about 10% of these
+# it's also missing from 13% of the extracts df
+# price is missing from 5%
+# potency is missing from 5%
+tests_numeric <- dplyr::filter(tests_numeric, !(is.na(price)), !(is.na(CBD)), !(is.na(Total)),
+                               # calculation for price per gram will be more accurate
+                               price>0, price<200)
+
+tests_numeric$inhalant_gen <- as.factor(tests_numeric$inhalant_gen)
+tests_numeric$inhalant_type <- as.factor(tests_numeric$inhalant_type)
+tests_numeric$lab_invtype <- as.factor(tests_numeric$lab_invtype)
+
+# test and train -------
+# Randomly select 20% of the data to be held out for model validation
+train <- sample(1:nrow(tests_numeric), 
+                round(0.2 * nrow(tests_numeric)))
+test <- setdiff(1:nrow(tests_numeric), train)
+
+tests.train <- tests_numeric[train,]
+tests.test <- tests_numeric[test,]
+# train.def <- tests_numeric$inhalant_gen[train]
+# test.def <- tests_numeric$inhalant_gen[test]
+
+# k means -----
+set.seed(427)
+km.out <- kmeans(tests.train[, 2:11], 5, nstart = 20)
+# join clusters back to df
+tests.train$cluster <- km.out$cluster
+
+table(tests.train$cluster, tests.train$inhalant_gen)
+table(tests.train$cluster, tests.train$inhalant_type)
+
+# very small groups in Groups 1 & 2, re-running without those
+outliers <- filter(tests.train, 
+                   inhalant_type=="Uncategorized", 
+                   cluster==1)
+tests.train <- filter(tests.train, !(inventoryparentid %in% outliers$inventoryparentid))
+
+km.out <- kmeans(tests.train[, 2:8], 4, nstart = 20)
+# join clusters back to df
+tests.train$cluster <- km.out$cluster
+table(tests.train$cluster, tests.train$inhalant_gen)
+table(tests.train$cluster, tests.train$inhalant_type)
+table(tests.train$cluster, tests.train$lab_invtype)
+
+
+
+
+
+
+
+# random forests -------
+library(randomForest)
+extracts.rf <- randomForest(inhalant_gen ~ bacteria + bile + coliforms + ecoli + yeast + moisture +
+                              solvent + price + CBD + Total, ntree=30, 
+                            data=tests_numeric, importance=TRUE)
+plot(road.rf)
+var.imp.road <- varImpPlot(road.rf)
+rownames(var.imp.road)[1:4]
+
+table(tests_numeric$inhalant_gen)
+
+
+
+
+
+
+
+
+
+
 
 
 extract_results <- labkey %>%
@@ -96,10 +205,12 @@ extract.test <- extract_results.sample[test,]
 train.def <- extract_results.sample$inhalant_gen[train]
 test.def <- extract_results.sample$inhalant_gen[test]
 
+
 # k nearest neighbors
 library(class)
-
-knn.pred <- knn(extract.train, extract.test, train.def, k=5)
+train.def <- tests_numeric$inhalant_gen[train]
+test.def <- tests_numeric$inhalant_gen[test]
+knn.pred <- knn(tests.train, tests.test, train.def, k=5)
 100 * sum(test.def == knn.pred)/100 
 
 
